3c33704c43a611a2de30ced18e7ba411fbbce44b
Brett <brett_matsumoto@yahoo.com>
2019-04-04 13:48:17 -1000

    reformatted

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,26 @@
-exports.largestPrimeFactor = function(n){
+exports.largestPrimeFactor = function (n) {
   var primeNumber = 0;
-  // do your work here
-
+  let numberArray = [];
+  while (n > 2) {
+    if (n % 2 === 0) {
+      n = n / 2;
+      numberArray.push(n);
+    }
+    if (n % 3 === 0) {
+      n = n / 3;
+      numberArray.push(n);
+    }
+    if (n % 5 === 0) {
+      n = n / 5;
+      numberArray.push(n);
+    }
+    if (n % 7 === 0) {
+      n = n / 7;
+      numberArray.push(n);
+    }
+    primeNumber = n;
+  }
+  console.log(numberArray);
   return primeNumber;
 };
+console.log(exports.largestPrimeFactor(20))
