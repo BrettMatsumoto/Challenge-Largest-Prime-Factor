6b503f32270093b787025798940c3929bc4adcdc
Brett <brett_matsumoto@yahoo.com>
2019-04-19 16:59:04 -1000

    removed console.log

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,25 +1,18 @@
 exports.largestPrimeFactor = function (n) {
   var primeNumber = 0;
   let numberArray = [];
-  while (n > 2) {
-    if (n % 2 === 0) {
-      n = n / 2;
-      numberArray.push(n);
+
+  for (let i = 0; i < n; i++) {
+    if (n % i === 0) {
+      numberArray.push(i);
+      n /= i;
     }
-    if (n % 3 === 0) {
-      n = n / 3;
-      numberArray.push(n);
-    }
-    if (n % 5 === 0) {
-      n = n / 5;
-      numberArray.push(n);
-    }
-    if (n % 7 === 0) {
-      n = n / 7;
-      numberArray.push(n);
-    }
-    primeNumber = n;
   }
-  return primeNumber;
+  for (let j = numberArray.length; j > 0; j--) {
+    if (numberArray[j] > numberArray[j-1]){
+      primeNumber = numberArray[j];
+    }
+  }
+  
+  return primeNumber
 };
-console.log(exports.largestPrimeFactor(20))
